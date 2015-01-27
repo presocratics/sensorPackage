be50b4dda3236993157d2c782b5642564dfbf667
Martin Miller <miller7@illinois.edu>
2015-01-27 10:26:33 -0600

    Modify span.sh to write unique lognames and add comments

--- src/pySpan/span.sh
+++ src/pySpan/span.sh
@@ -1,16 +1,35 @@
 #! /bin/sh
+# span.sh
+# 1/27/2015
+# Martin Miller
+# Runs span.py and starts logging. Generates a logname based on the current time
+# to prevent overwrites.
+
 device=/dev/ttyUSB0
+screenrc=/tmp/spanpy-screenrc
+
+## check if device is connected
 if [ ! -e $device ]
 then
     echo "No device"
     exit 1
 fi
+
+## run span and check if it worked.
 sudo python span.py $device
 if [ ! $? -eq 0 ]
-do
+then
     echo "Span failed."
     echo "If it's 'resource temporarily unavailable, or something about 'readiness to read'"
     echo "Retrying in a few seconds usually helps."
     exit 1
-done
-sudo screen -L $device 115200
+fi
+date=`date -I'minutes'`
+if [ ! $? -eq 0 ]
+then
+    echo "`date` failed."
+    exit 1
+fi
+# Create a temp screenrc to set the logfile name
+echo "logfile ${date}.log" | tee $screenrc
+sudo screen -c $screenrc -L $device 115200

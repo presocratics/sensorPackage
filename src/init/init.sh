6edb4c300430cb75d7f17a8e7ecdd13a2949dba5
Martin Miller <miller7@illinois.edu>
2016-06-06 17:20:47 -0500

    Update init.sh to fit into syncTime framework.
    User will run init.sh and feed it into syncTime, which will then get more data
    from the standard route. This will need some more tweaking to actually work.

--- src/init/init.sh
+++ src/init/init.sh
@@ -10,7 +10,9 @@ USB1=/dev/ttyUSB1
 echo unlogall > $USB1
 echo eventoutcontrol mark1 disable > $USB1
 
-sleep 1
+grabframe -1 | cut -d, -f2,3,4 > cam.txt &
+
+sleep 3
 
 echo log eventincontrol mark2 enable > $USB1
 echo log usb1 mark2time onnew > $USB1
@@ -20,7 +22,5 @@ sleep 1
 
 echo eventoutcontrol mark1 disable > $USB1
 
-camtime=$(grabframe -1 | head -1 | awk -F, '{printf("%0.5f\n", $3*10^-7)}')
-gpstime=$(<$USB0 head -n1| cut -d' ' -f6,7 | awk '{printf("%0.3f\n", 315964800 - 17 + 604800*$1 + $2)}')
-echo $gpstime - $camtime | bc
+<$USB0 head -n1 | cut -d, -f6,7 > gps.txt
 

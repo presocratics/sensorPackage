31b6dd2a432da9ebd82c521ed153b0232d621b4a
Martin Miller <miller7@illinois.edu>
2016-06-08 17:22:57 -0500

    Timestamps in GPS seconds only.
    Wrote a function to convert gpswk,sec to the total number of seconds. This makes
    removes the burden on downstream programs from having to know what gpswk is. In
    this way, they can handle either UTC or GPS seconds, or any other seconds based
    clock notion in the same manner.

--- src/live/span2slam.sh
+++ src/live/span2slam.sh
@@ -8,7 +8,7 @@
 rm -f /tmp/mark2time.ff
 mkfifo /tmp/mark2time.ff
 
-~/ARC/sensorPackage/src/syncTime/syncTime.py /tmp/mark2time.ff <(cut -d, -f2,3,4 $1) > img.txt &
+syncTime.py /tmp/mark2time.ff <(cut -d, -f2,3 $1) > img.txt &
 
 awk -F, 'function euler2qbw(roll,pitch,yaw,q) {
              q[0] = cos(roll/2)*cos(pitch/2)*cos(yaw/2)+sin(roll/2)*sin(pitch/2)*sin(yaw/2)
@@ -16,21 +16,26 @@ awk -F, 'function euler2qbw(roll,pitch,yaw,q) {
              q[2] = cos(roll/2)*cos(pitch/2)*sin(yaw/2)-sin(roll/2)*sin(pitch/2)*cos(yaw/2)
              q[3] = sin(roll/2)*cos(pitch/2)*cos(yaw/2)-cos(roll/2)*sin(pitch/2)*sin(yaw/2)
          }
-         
+
+         function gps2gpssec(wk,sec,        gpssec) {
+            gpssec = 604800*wk+sec
+            return gpssec
+         }
 
          BEGIN  { OFS="," }
          # Process RAWIMUS 
-         /^40,325/ {printf("%d,%f,ACC,%0.9f,%0.9f,%0.9f\n", $5,$6,
+         /^40,325/ {printf("%f,ACC,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),
          -200*200*2^-31*$9,
          200*200*2^-31*$10,
          -200*200*2^-31*$8) } 
 
-         /^40,325/ {printf("%d,%f,ANG,%0.9f,%0.9f,%0.9f\n",  $5,$6,
+         /^40,325/ {printf("%f,ANG,%0.9f,%0.9f,%0.9f\n",  gps2gpssec($5,$6),
          -200*720*2^-31*$12,
          200*720*2^-31*13,
          -200*720*2^-31*$11)}
          # Process RAWIMUS for calibration
-         /^40,325/ {printf("%f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n", $6,
+         /^40,325/ {printf("%f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n", 
+         gps2gpssec($5,$6),
          -200*720*2^-31*$12,
          200*720*2^-31*13,
          -200*720*2^-31*$11,
@@ -40,21 +45,21 @@ awk -F, 'function euler2qbw(roll,pitch,yaw,q) {
 
 
          # Process INSPVAS
-         /^88,508/ {printf("%d,%f,POS,%0.9f,%0.9f,%0.9f\n", $5,$6,$7,$8,$9)} 
-         /^88,508/ {printf("%d,%f,VEL,%0.9f,%0.9f,%0.9f\n", $5,$6,$10,$11,-$12)}
+         /^88,508/ {printf("%f,POS,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),$7,$8,$9)} 
+         /^88,508/ {printf("%f,VEL,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),$10,$11,-$12)}
          /^88,508/ {
             euler2qbw($13,$14,$15,q);
-            printf("%d,%f,QUAT,%0.9f,%0.9f,%0.9f,%0.9f\n",
-            $5,$6,q[0],q[1],q[2],q[3])
+            printf("%f,QUAT,%0.9f,%0.9f,%0.9f,%0.9f\n",
+            gps2gpssec($5,$6),q[0],q[1],q[2],q[3])
          }
 
          # Process CORRIMUDATAS
-         /^60,813/ {printf("%d,%f,ACC,%0.9f,%0.9f,%0.9f\n", $5,$6,
+         /^60,813/ {printf("%f,ACC,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),
          200*$11,200*$10,-200*$12)}
-         /^60,813/ {printf("%d,%f,ANG,%0.9f,%0.9f,%0.9f\n", $5,$6,
+         /^60,813/ {printf("%f,ANG,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),
          200*$8,200*$7,-200*$9)}
 
          # Process MARK2TIME
-         /^616/ {print $8,$9*1e-3 > "/tmp/mark2time.ff" }' 
+         /^616/ {print gps2gpssec($8,$9*1e-3) > "/tmp/mark2time.ff" }' 
 
 rm -f /tmp/mark2time.ff

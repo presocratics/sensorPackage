71cda6d205832eb5ef1d089346f36a7bbd51ccf7
Martin Miller <miller7@illinois.edu>
2016-06-14 15:00:58 -0500

    Fix quaternion calculation and write output to file.
    euler2qbw() expects attitude in radians, but the span outputs in degrees. The
    conversion from degrees to radians was added into the function.
    
    When running live, there were issues with the output being written to stdout
    that don't appear when writing directly to a file, so we are now writing to a
    timestamped file.

--- src/live/span2slam.sh
+++ src/live/span2slam.sh
@@ -11,11 +11,14 @@ mkfifo /tmp/mark2time.ff
 syncTime /tmp/mark2time.ff <(tail -n+1 -f $1 | cut -d, -f2,3 ) | tee img.txt | awk -F, \
 '{printf("%s,%d\n", $3, int(10^9*$1))}' > clib.txt &
 
-awk -F, 'function euler2qbw(roll,pitch,yaw,q) {
-             q[0] = cos(roll/2)*cos(pitch/2)*cos(yaw/2)+sin(roll/2)*sin(pitch/2)*sin(yaw/2)
-             q[1] = cos(roll/2)*sin(pitch/2)*cos(yaw/2)+sin(roll/2)*cos(pitch/2)*sin(yaw/2)
-             q[2] = cos(roll/2)*cos(pitch/2)*sin(yaw/2)-sin(roll/2)*sin(pitch/2)*cos(yaw/2)
-             q[3] = sin(roll/2)*cos(pitch/2)*cos(yaw/2)-cos(roll/2)*sin(pitch/2)*sin(yaw/2)
+awk -F, 'function euler2qbw(roll,pitch,yaw,q,    rd, pd, yd) {
+             rd = roll*0.017453292519943295
+             pd = pitch*0.017453292519943295
+             yd = yaw*0.017453292519943295
+             q[0] = cos(rd/2)*cos(pd/2)*cos(yd/2)+sin(rd/2)*sin(pd/2)*sin(yd/2)
+             q[1] = cos(rd/2)*sin(pd/2)*cos(yd/2)+sin(rd/2)*cos(pd/2)*sin(yd/2)
+             q[2] = cos(rd/2)*cos(pd/2)*sin(yd/2)-sin(rd/2)*sin(pd/2)*cos(yd/2)
+             q[3] = sin(rd/2)*cos(pd/2)*cos(yd/2)-cos(rd/2)*sin(pd/2)*sin(yd/2)
          }
 
          function gps2gpssec(wk,sec,        gpssec) {
@@ -23,17 +26,19 @@ awk -F, 'function euler2qbw(roll,pitch,yaw,q) {
             return gpssec
          }
 
-         BEGIN  { OFS="," }
+         BEGIN  { OFS=","
+                  fout=strftime("%d%m%Y-%T",systime()) ".gps"
+         }
          # Process RAWIMUS 
          /^40,325/ {printf("%f,ACC,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),
          -200*200*2^-31*$9,
          200*200*2^-31*$10,
-         -200*200*2^-31*$8) } 
+         -200*200*2^-31*$8) > fout } 
 
          /^40,325/ {printf("%f,ANG,%0.9f,%0.9f,%0.9f\n",  gps2gpssec($5,$6),
          -200*720*2^-31*$12,
          200*720*2^-31*13,
-         -200*720*2^-31*$11)}
+         -200*720*2^-31*$11) > fout}
          # Process RAWIMUS for calibration
          /^40,325/ {printf("%d,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n", 
          int(1e9*gps2gpssec($5,$6)),
@@ -46,19 +51,21 @@ awk -F, 'function euler2qbw(roll,pitch,yaw,q) {
 
 
          # Process INSPVAS
-         /^88,508/ {printf("%f,POS,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),$7,$8,$9)} 
-         /^88,508/ {printf("%f,VEL,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),$10,$11,-$12)}
+         /^88,508/ {printf("%f,POS,%0.9f,%0.9f,%0.9f\n",
+         gps2gpssec($5,$6),$7,$8,$9) > fout} 
+         /^88,508/ {printf("%f,VEL,%0.9f,%0.9f,%0.9f\n",
+         gps2gpssec($5,$6),$10,$11,-$12) > fout}
          /^88,508/ {
             euler2qbw($13,$14,$15,q);
             printf("%f,QUAT,%0.9f,%0.9f,%0.9f,%0.9f\n",
-            gps2gpssec($5,$6),q[0],q[1],q[2],q[3])
+            gps2gpssec($5,$6),q[0],q[1],q[2],q[3]) > fout
          }
 
          # Process CORRIMUDATAS
          /^60,813/ {printf("%f,ACC,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),
-         200*$11,200*$10,-200*$12)}
+         200*$11,200*$10,-200*$12) > fout}
          /^60,813/ {printf("%f,ANG,%0.9f,%0.9f,%0.9f\n", gps2gpssec($5,$6),
-         200*$8,200*$7,-200*$9)}
+         200*$8,200*$7,-200*$9) > fout}
 
          # Process MARK2TIME
          /^616/ {print gps2gpssec($8,$9*1e-3) > "/tmp/mark2time.ff" }' 

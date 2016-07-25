e8f2c78ab6697eaaeecc965ebc5cc58eebe2fe20
Martin Miller <miller7@illinois.edu>
2016-07-25 15:24:05 -0500

    Add CRC verification to pybin output.
    When the CRC hash does not match the message and an error is written to stderr.
    All good messages are written to stdout.

--- src/pybin/pybin.py
+++ src/pybin/pybin.py
@@ -23,38 +23,63 @@ def getStart(ser):
             curstate=0
     if byte==b'\x13':
         isShort=True
-    return isShort
+    return byte,isShort
 
 def readHeader(ser,isShort=False):
     """Reads the header of the message"""
     if isShort:
         format='=BHHL'
-        header_data=ser.read(9)
+        raw_header=ser.read(9)
         fields=['Message_Length','Message_ID','Week_Number','Milliseconds']
     else:
         header_length=ser.read(1)
         num_to_read=ord(header_length)-4
         format='=HcBHHBcHLLHH'
-        header_data=ser.read(num_to_read)
+        raw_header=ser.read(num_to_read)
         fields=['Message_ID','Message_Type','Port_Address','Message_Length',
                 'Sequence','Idle_Time','Time_Status','Week','ms','Receiver_Status',
                 'Reserved','Receiver_SW_Version']
-    header_data=struct.unpack(format,header_data)
+    header_data=struct.unpack(format,raw_header)
     tuple=namedtuple('header',fields)
+    if not isShort:
+        raw_header=header_length+raw_header
 
-    return tuple._make(header_data)
+    return raw_header,tuple._make(header_data)
 
 def read_message(ser,msg_len,format):
     """Reads according to format"""
-    message=ser.read(msg_len)
-    message=struct.unpack(format,message)
-    return message
+    rawmessage=ser.read(msg_len)
+    message=struct.unpack(format,rawmessage)
+    return rawmessage,message
 
 def read_CRC(ser):
-    """Reads 32-bin CRC value"""
+    """Reads 32-bit CRC value"""
     crc=ser.read(4)
     crc=struct.unpack('=4s',crc)
     return crc
+
+def CRC32Value(ulCRC):
+    """Calculate intermediate value for calc_CRC()"""
+    CRC32_POLYNOMIAL=0xEDB88320
+    for j in range(8):
+        if ulCRC & 1:
+            ulCRC = (ulCRC>>1) ^ CRC32_POLYNOMIAL
+        else:
+            ulCRC>>=1
+    return ulCRC
+
+def calc_CRC(msg):
+    """Calculates 32-bit CRC value"""
+    L=int(len(msg))
+    ulCRC=0
+    for i in range(L):
+        byte=msg[i:i+1]
+        byte=int.from_bytes(byte,'big')
+        ulTemp1 = (ulCRC>>8) & 0x00ffffff
+        ulTemp2 = CRC32Value((ulCRC ^ byte) & 0xff)
+        ulCRC = ulTemp1 ^ ulTemp2
+    return ulCRC.to_bytes(4,'little')
+
 
 def get_defines():
     """Returns constant data"""
@@ -203,16 +228,29 @@ def main():
     else:
         ser=serial.Serial("/dev/ttyUSB0",baudrate=115200)
     while True:
-        isShort=getStart(ser)
-        header=readHeader(ser,isShort)
+        rawstart,isShort=getStart(ser)
+        rawhdr,header=readHeader(ser,isShort)
         msg_name=message_ids[header.Message_ID]
         format=message_fmts[msg_name]
-        message_data=read_message(ser,header.Message_Length,format)
+        rawmsg,message_data=read_message(ser,header.Message_Length,format)
+        crc=read_CRC(ser)
+        if rawstart==b'\x13':
+            raw=b'\xaa\x44\x13'
+        else:
+            raw=b'\xaa\x44\x12'
+        raw+=rawhdr
+        raw+=rawmsg
+        crc_calcd=calc_CRC(raw)
+
         MsgTuple=namedtuple(msg_name,fields[msg_name])
         message=MsgTuple._make(message_data)
         output=list(header)
         output.extend(list(message))
-        print(",".join(map(str,output)))
+
+        if crc_calcd==crc[0]:
+            print(",".join(map(str,output)))
+        else:
+            print("Error: ", ",".join(map(str,output)),file=sys.stderr)
 
 if __name__=='__main__':
     main()

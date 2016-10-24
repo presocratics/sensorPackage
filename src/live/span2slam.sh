3181142ad368da163e8c89e1130878ad6cfde422
Martin Miller <miller7@illinois.edu>
2016-10-24 09:02:20 -0500

    Fix span2slam inscov error where attitude and velocity cov were transposed.

--- src/live/span2slam.sh
+++ src/live/span2slam.sh
@@ -69,9 +69,9 @@ gawk -F, -v fname=$1 'function euler2qbw(roll,pitch,yaw,q,    rd, pd, yd) {
          # Process inscovs
          /^228,320,/ {printf("%f,COVP,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n",
              gps2gpssec($5,$6),$7,$8,$9,$10,$11,$12,$13,$14,$15) > fout }
-         /^228,320,/ {printf("%f,COVV,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n",
-             gps2gpssec($5,$6),$16,$17,$18,$19,$20,$21,$22,$23,$24) > fout }
          /^228,320,/ {printf("%f,COVA,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n",
+             gps2gpssec($5,$6),$16,$17,$18,$19,$20,$21,$22,$23,$24) > fout }
+         /^228,320,/ {printf("%f,COVV,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f,%0.9f\n",
              gps2gpssec($5,$6),$25,$26,$27,$28,$29,$30,$31,$32,$33) > fout }
 
          # Process MARK2TIME
